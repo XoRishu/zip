44fb34d2b619d801df2288e8fc5b69053406b521
Rishu <115208147+XoRishu@users.noreply.github.com>
2023-06-12 12:11:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,30 +28,56 @@ def unzip(update: Update, context: CallbackContext):
             os.remove('temp.zip')
             
             # Get the list of files in the unzipped folder
-            files = os.listdir('unzipped')
-            file_count = len(files)
-            
-            # Prepare the response
-            response_text = f"Unzipped {file.file_name}:\n"
-            response_text += f"Total files: {file_count}\n"
-            response_text += "Contents:"
-            
-            # Send the response
-            update.message.reply_text(response_text)
-            
-            # Send the contents as compressed images or videos
-            for file_name in files:
-                file_path = os.path.join('unzipped', file_name)
-                if file_name.lower().endswith(('.jpg', '.jpeg', '.png')):
-                    context.bot.send_photo(update.message.chat_id, photo=open(file_path, 'rb'))
-                elif file_name.lower().endswith(('.mp4', '.avi', '.mkv')):
-                    context.bot.send_video(update.message.chat_id, video=open(file_path, 'rb'))
-                else:
-                    # Send other file types as documents
-                    context.bot.send_document(update.message.chat_id, document=open(file_path, 'rb'))
-            
-            # Remove the unzipped folder
-            shutil.rmtree('unzipped')
+            files = os# Handler for when a zip file is sent
+def unzip(update: Update, context: CallbackContext):
+    # Check if the message has a document
+    if update.message.document:
+        file = update.message.document
+        # Check if the document is a zip file
+        if file.file_name.endswith('.zip'):
+            # Check the file size
+            if file.file_size <= 50 * 1024 * 1024:  # Adjust the size limit as needed (e.g., 50 MB)
+                # Download the zip file
+                file_id = file.file_id
+                new_file = context.bot.get_file(file_id)
+                new_file.download('temp.zip')
+                
+                # Unzip the file
+                with zipfile.ZipFile('temp.zip', 'r') as zip_ref:
+                    zip_ref.extractall('unzipped')
+                
+                # Remove the temporary zip file
+                os.remove('temp.zip')
+                
+                # Get the list of files in the unzipped folder
+                files = os.listdir('unzipped')
+                file_count = len(files)
+                
+                # Prepare the response
+                response_text = f"Unzipped {file.file_name}:\n"
+                response_text += f"Total files: {file_count}\n"
+                response_text += "Contents:"
+                
+                # Send the response
+                update.message.reply_text(response_text)
+                
+                # Send the contents as compressed images or videos
+                for file_name in files:
+                    file_path = os.path.join('unzipped', file_name)
+                    if file_name.lower().endswith(('.jpg', '.jpeg', '.png')):
+                        context.bot.send_photo(update.message.chat_id, photo=open(file_path, 'rb'))
+                    elif file_name.lower().endswith(('.mp4', '.avi', '.mkv')):
+                        context.bot.send_video(update.message.chat_id, video=open(file_path, 'rb'))
+                    else:
+                        # Send other file types as documents
+                        context.bot.send_document(update.message.chat_id, document=open(file_path, 'rb'))
+                
+                # Remove the unzipped folder
+                shutil.rmtree('unzipped')
+            else:
+                update.message.reply_text("The zip file is too large. Please send a smaller file.")
+        else:
+            update.message.reply_text("The file is not a zip file. Please send a valid zip file.")
 
 # Handler for any other message
 def unknown(update: Update, context: CallbackContext):
