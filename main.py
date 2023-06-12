9bb7b506e382335618fb515e96e7da95cb15b11e
Rishu <115208147+XoRishu@users.noreply.github.com>
2023-06-12 12:06:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Send a zip file with the /unzip command to extract its contents.")
 
 def unzip(update, context):
-    # Check if the message has a zip file
+    # Check if the message has a document
+    if not update.message.document:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please send a document.")
+        return
+
+    # Check if the document is a zip file
     if update.message.document.mime_type != 'application/zip':
         context.bot.send_message(chat_id=update.effective_chat.id, text="Please send a zip file.")
         return
-    
+
     file_id = update.message.document.file_id
     file_name = update.message.document.file_name
-    
+
     # Download the zip file
     file_path = context.bot.get_file(file_id).download(file_name)
-    
+
     # Extract the contents
     extracted_files = []
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
@@ -27,7 +32,7 @@ def unzip(update, context):
             # Extract each file from the zip
             zip_ref.extract(file, './temp')
             extracted_files.append(file)
-    
+
     # Prepare and send the extracted files
     for file in extracted_files:
         file_path = os.path.join('./temp', file)
@@ -39,10 +44,10 @@ def unzip(update, context):
                 context.bot.send_video(chat_id=update.effective_chat.id, video=f, caption=file)
             else:
                 context.bot.send_document(chat_id=update.effective_chat.id, document=f, caption=file)
-    
+
     # Clean up temporary directory
     os.system('rm -r ./temp')
-    
+
     # Delete the downloaded zip file
     os.remove(file_path)
 
